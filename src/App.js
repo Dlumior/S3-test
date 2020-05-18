1e9d68485ba9cb583ba441f757440cd1d8ce2b9f
TyS <josue.llaque@pucp.edu.pe>
2020-05-18 03:31:41 -0500

    Vistas de Registrar Tutores y alumnos
    Falta combobox de programa y sus toques

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import Home from "./pages/Home/Home.js";
 import Administrador from "./pages/Administrador/Administrador.js";
 import Coordinador from "./pages/Administrador/Coordinador.js";
 
+import regTutor from "./pages/Coordinador/RegistrarTutor.js";
+import regAlumno from "./pages/Coordinador/RegistrarAlumno.js";
+
 function App() {
   return (
     <Provider store={store}>
@@ -18,8 +21,8 @@ function App() {
             <Route exact path="/" component={Home} />
             <Route exact path="/administrador" component={Administrador} />
             <Route exact path="/ListarCoordinador" component={Coordinador} />
-            
-
+            <Route exact path="/RegistrarTutor" component={regTutor} />
+            <Route exact path="/RegistrarAlumno" component={regAlumno} />
           </div>
         </Router>
       </ThemeProvider>
