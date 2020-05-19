f2771bb432b312c97500c71cfcf865d07178f819
Jennifer <jennifercrisben@gmail.com>
2020-05-18 22:33:37 -0500

    RegistroCoordinador

--- src/pages/Administrador/Coordinador.js
+++ src/pages/Administrador/Coordinador.js
@@ -3,6 +3,7 @@ import React from "react";
 // import Tabla from "../../components/Administrador/Tabla";
 import NombrePrincipal from "../../components/Shared/NombrePrincipal";
 import TablaCoordinador from "../../components/Administrador/TablaCoordinador";
+import RegistrarCoordinador from "../../components/Administrador/RegistrarCoordinador";
 
 const Coordinador = () => {
   return (
@@ -10,6 +11,9 @@ const Coordinador = () => {
       <NombrePrincipal titulo="Coordinadores" />
 
       <TablaCoordinador titulo="Coordinadores" />
+
+      <RegistrarCoordinador/>
+
     </div>
   );
 };
