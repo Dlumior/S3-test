f6d19dbe54b94b859d184bef915a40d8ed0595d6
X Æ A-12 <ariana.quispep@pucp.edu.pe>
2020-05-24 00:22:42 -0500

    honk honk

--- src/pages/Tutor/Calendario.js
+++ src/pages/Tutor/Calendario.js
@@ -1,21 +1,25 @@
 import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
 
+import { Grid } from "@material-ui/core";
+import Paper from '@material-ui/core/Paper';
 import ImgTutor from "../../components/Tutor/tutor.png";
 import CabeceraPerfil from "../../components/Shared/CabeceraPerfil";
-import ArrowForwardIosRoundedIcon from '@material-ui/icons/ArrowForwardIosRounded';
 import ArrowBackIosRoundedIcon from '@material-ui/icons/ArrowBackIosRounded';
+import ArrowForwardIosRoundedIcon from '@material-ui/icons/ArrowForwardIosRounded';
 
 const useStyles = makeStyles((theme) => ({
-    flechita: {
-      margin: theme.spacing(1),
+    diasclase: {
+      height: 400,
     },
-    Mesesito: {
-      marginRight: theme.spacing(1),
-    },
+    texto_dias: {
+        textAlign: "center",
+    }
   }));
 
 const Calendario = () => {
+    const classes = useStyles();
+
     return (
         <div>
             <CabeceraPerfil titulo="Tutor" 
@@ -23,30 +27,52 @@ const Calendario = () => {
                             imagen={ImgTutor}
             />
             <div>
-                <ArrowBackIosRoundedIcon color="primary" className = "flechita"/>
-                <h2 className = "Mesesito">MARZO</h2>
-                <ArrowForwardIosRoundedIcon color="primary" className="flechita"/>
-                <div>
-                    <div class="month"> 
-                        <ul>
-                            <li class="prev">&#10094;</li>
-                            <li class="next">&#10095;</li>
-                            <li>August<br/><span>2017</span></li>
-                        </ul>
-                    </div>
-
-                    <ul class="weekdays">
-                    <li>Mo</li>
-                    <li>Tu</li>
-                    <li>We</li>
-                    <li>Th</li>
-                    <li>Fr</li>
-                    <li>Sa</li>
-                    <li>Su</li>
-                    </ul>
-                    </div>
-                </div>
+                <Grid container xs={12} spacing={5} justify="center" alignItems="center">
+                    <Grid item>
+                        <ArrowBackIosRoundedIcon color="primary" />
+                    </Grid>
+                    <Grid item>
+                        <h2>MARZO</h2>
+                    </Grid>
+                    <Grid item>
+                        <ArrowForwardIosRoundedIcon color="primary" />
+                    </Grid>
+                </Grid>
+                
+                <Grid container xs={12} spacing={5} justify="center" alignItems="center">
+                    <Grid item xs={2}>
+                        <Paper elevation={2} className={classes.diasclase}>
+                            <div  className={classes.texto_dias}>Lunes</div>
+                        </Paper>
+                    </Grid>  
+                    <Grid item xs={2}>
+                        <Paper elevation={2} className={classes.diasclase}>
+                            <div className={classes.texto_dias}>Martes</div>
+                        </Paper>
+                    </Grid> 
+                    <Grid item xs={2}>
+                        <Paper elevation={2} className={classes.diasclase}>
+                            <div className={classes.texto_dias}>Miércoles</div>
+                        </Paper>
+                    </Grid>  
+                    <Grid item xs={2}>
+                        <Paper elevation={2} className={classes.diasclase}>
+                            <div className={classes.texto_dias}>Jueves</div>
+                        </Paper>
+                    </Grid>  
+                    <Grid item xs={2}>
+                        <Paper elevation={2} className={classes.diasclase}>
+                            <div className={classes.texto_dias}>Viernes</div>
+                        </Paper>
+                    </Grid> 
+                    <Grid item xs={2}>
+                        <Paper elevation={2} className={classes.diasclase}>
+                            <div className={classes.texto_dias}>Sábado</div>
+                        </Paper>
+                    </Grid> 
+                </Grid>
             </div>
+        </div>
     );
 };
 
