0c1ece98af2017d92edd3f0e68f1f74609fdee19
TyS <josue.llaque@pucp.edu.pe>
2020-05-30 00:04:06 -0500

    cositasRicasEnAlumno
    TySwasHere

--- src/pages/Alumno/Alumno.js
+++ src/pages/Alumno/Alumno.js
@@ -3,17 +3,17 @@ import { Route } from "react-router-dom";
 import BarraNavegacion from "../../components/Alumno/BarraNavegacion.js";
 import Perfil from "./Perfil.js";
 import AgendarCita from "./AgendarCita.js";
-
+import MisCitas from "./MisCitas.js";
 
 const Alumno = () => {
   return (
     <div>
       <br></br> <br></br> <br></br>
-      <Route  exact path={"/alumno/"} component={Perfil} />
+      <Route  exact path={"/alumno/"} component={MisCitas} />
       <BarraNavegacion>
         <Route exact path={"/alumno/perfil"} component={Perfil} />
         <Route exact path={"/alumno/agendarCita"} component={AgendarCita} />
-
+        <Route exact path={"/alumno/misCitas"} component={MisCitas} />
       </BarraNavegacion>
     </div>
   );
