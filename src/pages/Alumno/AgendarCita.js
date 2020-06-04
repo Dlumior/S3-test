5d4528697a8d2163703949704875fc00ee9c964b
Jose <joses@karbondb.com>
2020-06-04 13:29:10 -0500

    inicio cita alumno

--- src/pages/Alumno/AgendarCita.js
+++ src/pages/Alumno/AgendarCita.js
@@ -1,83 +1,49 @@
 import React, { Component } from "react";
 import NombrePrincipal from "../../components/Shared/NombrePrincipal";
 import FrmSolicitarTutorTipoII from "../../components/Alumno/FrmSolicitarTutorTipoII.js";
-
+import TabProceso from "../../components/Coordinador/Tutorias/TabProceso";
+import FormNuevaTutoria from "../../components/Coordinador/Tutorias/FormNuevaTutoria";
 
 const titulo = "Escoja un tutor para agendar una cita ";
-
-
-/*
-const useStyles = makeStyles((theme) => ({
-    margin: {
-        margin: theme.spacing(1),
-    },
-}));
-const classes = useStyles();
-const theme = createMuiTheme({
-    palette: {
-        primary: green,
-    },
-});
-*/
-
-
 class AgendarCita extends Component {
-    constructor() {
-        super();
-        this.state = {
-
-        };
-
-        /* 
-        this.handleOnClick = this.handleOnClick.bind(this);
-        this.handleOnChangePrograma = this.handleOnChangePrograma.bind(this);
-        this.handleOnChange = this.handleOnChange.bind(this);
-
-                 async handleOnClick(e) {
-
-            //mostrar warning
-
-        }
-        this.handleOnClick = this.handleOnClick.bind(this);
-        */
-
+  constructor() {
+    super();
+    this.state = {
+      titulosTutoria: [, "Escoja un tutor para agendar una cita"],
+      procesos: [
+        {
+          regularTipo1: {
+            titulo: "Solicitud de Cita",
+            procesos: [
+              { index: 0, titulo: "Horarios Disponibles", proceso: FormNuevaTutoria,},
+              { index: 1, titulo: "Tutores", proceso: FrmSolicitarTutorTipoII },
+            ],
+          },
+          regularTipo2: {
+            titulo: "Escoja un tutor para agendar una cita",
+            procesos: [
+              { index: 0, titulo: "Tutores", proceso: FrmSolicitarTutorTipoII },
+            ],
+          },
+        },
+      ],
     };
+  }
 
-
-    render() {
-        return (
-            <div>
-                <NombrePrincipal titulo={titulo} />
-                {/*Aca habria una especia de if para ver que formulario abrir
+  render() {
+    return (
+      <div>
+        <NombrePrincipal titulo={titulo} />
+        {/*Aca habria una especia de if para ver que formulario abrir
                 de acuerdo al tipo de tutoria 
                 <FormSolicitarTuror tipo = idTipo... +o->
                 */}
-
-                <FrmSolicitarTutorTipoII />
-                {/*//Tipo II : tutoria INDIVIDUAL, tutor FIJO y SELECCIONADO */}
-
-            </div>
-        );
-    }
-
+        {/** exacto y lo unico que se debe reemlazar seria los procesos que van */}
+        <TabProceso procesos={this.state.procesos.regularTipo2} />
+        {/*<FrmSolicitarTutorTipoII />*/}
+        {/*//Tipo II : tutoria INDIVIDUAL, tutor FIJO y SELECCIONADO */}
+      </div>
+    );
+  }
 }
 export default AgendarCita;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
