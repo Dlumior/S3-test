d1f3e62c27d122f3020781bb6c6456d14c8b10d5
TyS <josue.llaque@pucp.edu.pe>
2020-06-10 03:34:34 -0500

    HIS33_35 solicitudes de citas
    ya muestra dialogo al hacer click en una cartilla <3

--- src/components/Alumno/AgendarCita/DisponibilidadCard.jsx
+++ src/components/Alumno/AgendarCita/DisponibilidadCard.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
-import { Grid, Chip, Paper, Typography, Button } from "@material-ui/core";
+import { Grid, Chip, Paper, Typography, Button, Dialog } from "@material-ui/core";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
 import ImagenCircular from "../../Shared/ImagenCircular";
+import FrmDialogoSolicitarTutor from "../FrmDialogoSolicitarTutor.js";
+
 const styles = {
   paper: {
     borderTop: "3px solid #3AAFA9",
@@ -11,82 +15,144 @@ const styles = {
   },
 };
 class DisponibilidadCard extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      tutores: {
+        columns: [{
+          title: "Nombre",
+          field: "nombre",
+        }],
+        data: [{ nombre: "" }]
+      }, //aqui va el nombre de la tablilla
+      open: false,
+
+    };
+
+    this.handleOnClick = this.handleOnClick.bind(this);
+    this.handleOnClose = this.handleOnClose.bind(this);
+  };
+
+
+  //=============================================================
+  handleOnClick() {
+    this.setState({ open: true });
+  }
+
+  handleOnClose() {
+    //console.log("ctm",this.state.openSolicitarTutor);
+    this.setState({ open: false });
+  }
+
+
+
+
   render() {
     const disponibilidad = this.props.disponibilidad;
     return (
       <>
-        <Paper style={styles.paper}>
-          {/** Encabezado Imagen y nombre */}
-          <Grid container spacing={2} style={styles.chip}>
-            <Grid item md={3} xs={3}>
-              <ImagenCircular src="https://www.w3schools.com/howto/img_avatar.png" />
+        <Button
+          onClick={this.handleOnClick}
+
+        >
+
+
+          <Paper style={styles.paper}>
+            {/** Encabezado Imagen y nombre */}
+            <Grid container spacing={2} style={styles.chip}>
+              <Grid item md={3} xs={3}>
+                <ImagenCircular src="https://www.w3schools.com/howto/img_avatar.png" />
+              </Grid>
+              <Grid item md={9} xs={9}>
+                <Typography
+                  variant="button"
+                  component="h4"
+                  style={styles.control}
+                  display="block"
+                  gutterBottom
+                >
+                  {disponibilidad.TUTOR.USUARIO.NOMBRE + " "}
+                </Typography>
+                <Typography
+                  variant="button"
+                  component="h3"
+                  style={styles.control}
+                  display="block"
+                  gutterBottom
+                >
+                  {disponibilidad.TUTOR.USUARIO.APELLIDOS}
+                </Typography>
+              </Grid>
             </Grid>
-            <Grid item md={9} xs={9}>
-              <Typography
-                variant="button"
-                component="h4"
-                style={styles.control}
-                display="block"
-                gutterBottom
-              >
-                {disponibilidad.TUTOR.USUARIO.NOMBRE + " "}
-              </Typography>
-              <Typography
-                variant="button"
-                component="h3"
-                style={styles.control}
-                display="block"
-                gutterBottom
-              >
-                {disponibilidad.TUTOR.USUARIO.APELLIDOS}
-              </Typography>
-            </Grid>
-          </Grid>
-          {/* Las horas */}
-          <Grid container spacing={0} alignContent='stretch'>
-            <Grid item md={6} xs={6}>
-              <Typography
-                variant="button"
-                component="h2"
-                style={styles.chip}
-                display="block"
-                gutterBottom
-              >
-                <strong>Hora Inicio</strong>
-              </Typography>
-            </Grid>
-            <Grid item md={6} xs={6}>
-              <Chip
-                label={disponibilidad.HORA_INICIO}
-                color="primary"
-                size="small"
-                variant="outlined"
-              />
+            {/* Las horas */}
+            <Grid container spacing={0} alignContent='stretch'>
+              <Grid item md={6} xs={6}>
+                <Typography
+                  variant="button"
+                  component="h2"
+                  style={styles.chip}
+                  display="block"
+                  gutterBottom
+                >
+                  <strong>Hora Inicio</strong>
+                </Typography>
+              </Grid>
+              <Grid item md={6} xs={6}>
+                <Chip
+                  label={disponibilidad.HORA_INICIO}
+                  color="primary"
+                  size="small"
+                  variant="outlined"
+                />
+              </Grid>
+
+              <Grid item md={6} xs={6}>
+                <Typography
+                  variant="button"
+                  component="h2"
+                  style={styles.chip}
+                  display="block"
+                  gutterBottom
+                >
+                  <strong>Hora Fin</strong>
+                </Typography>
+              </Grid>
+              <Grid item md={6} xs={6}>
+                <Chip
+                  label={disponibilidad.HORA_FIN}
+                  color="primary"
+                  size="small"
+                  variant="outlined"
+                />
+              </Grid>
+              {/**fin minicontainer */}
             </Grid>
 
-            <Grid item md={6} xs={6}>
-              <Typography
-                variant="button"
-                component="h2"
-                style={styles.chip}
-                display="block"
-                gutterBottom
-              >
-                <strong>Hora Fin</strong>
-              </Typography>
-            </Grid>
-            <Grid item md={6} xs={6}>
-              <Chip
-                label={disponibilidad.HORA_FIN}
-                color="primary"
-                size="small"
-                variant="outlined"
-              />
-            </Grid>
-            {/**fin minicontainer */}
-          </Grid>
-          
-        </Paper><br/>
+          </Paper>
+        </Button>
+
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleOnClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogContent>
+            <FrmDialogoSolicitarTutor />
+          </DialogContent>
+
+          <DialogActions>
+            <Button
+              size="large"
+              variant="contained"
+              color="primary"
+              onClick={this.handleOnClose}                        >
+              Solicitar Cita
+                        </Button>
+          </DialogActions>
+        </Dialog>
+
+        <br />
       </>
     );
   }
