b3f35dd19a0d305bf1c69fc591a7d0c92de0f30d
X Æ A-12 <ariana.quispep@pucp.edu.pe>
2020-06-16 23:37:18 -0500

    honk honk

--- src/pages/Tutor/Sesiones.js
+++ src/pages/Tutor/Sesiones.js
@@ -7,7 +7,7 @@ import FrmSolicitarCitaTutor_granito from "../../components/Alumno/FrmSolicitarC
 import CalendarioCitas from "../../components/Alumno/AgendarCita/CalendarioCitas";
 import { UserContext, getUser } from "../../Sesion/Sesion";
 
-let yo = getUser();
+
 class Sesiones extends Component {
     constructor() {
       super();
@@ -17,7 +17,7 @@ class Sesiones extends Component {
             titulo: "Solicite una cita según las disponibilidades disponibles",
             procesos: [
               { index: 0, titulo: "Horarios Disponibles", proceso: ()=><CalendarioCitas servicio={"/api/listaSesiones/"+
-              yo.usuario.ID_USUARIO +"/"} tipo="cita"/> },
+                getUser().usuario.ID_USUARIO +"/"} tipo="cita"/> },
             ],
           },
         ],
