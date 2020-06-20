e132ddba1a8a37380adcb36387a366ed4492cafc
Luis <ld_ramirez27@outlook.com>
2020-06-19 23:46:52 -0500

    Perfiles de Alumno. coordinador y tutor finalizados

--- src/pages/Coordinador/Perfil.js
+++ src/pages/Coordinador/Perfil.js
@@ -1,9 +1,7 @@
 import React, { useState, useRef } from "react";
 import { getUser } from "../../Sesion/Sesion";
 import Datos from "../../components/Coordinador/Datos";
-//import Cabecera from "../../components/Coordinador/Cabecera.js";
 import { Grid, makeStyles } from "@material-ui/core";
-
 import LuisR from "../../components/Coordinador/luisRios.png";
 import CabeceraPerfil from "../../components/Shared/CabeceraPerfil.js";
 import { POST } from "../../Conexion/Controller";
@@ -20,49 +18,38 @@ const Perfil = (props) => {
   const dir = useRef(null);
   const tel = useRef(null);
 
-  const handleEdit = async (e) => {
+  const handleEdit = (e) => {
     setIsEdit(true);
-    console.log(dir.current.value);
-    console.log(tel.current.value);
-    const datos = {};
-    if (getUser().usuario.ROL_X_USUARIO_X_PROGRAMAs[0].ROL.ID_ROL === 2) {
-      datos = {
-        ID: getUser().usuario.ID_USUARIO,
-        NOMBRE: getUser().usuario.NOMBRE,
-        APELLIDOS: getUser().usuario.APELLIDOS,
-        CODIGO: getUser().usuario.CODIGO,
-        CORREO: getUser().usuario.CORREO,
-        TELEFONO: tel.current.value,
-        DIRECCION: dir.current.value,
-        USUARIO: getUser().usuario.USUARIO,
-        IMAGEN: getUser().usuario.IMAGEN,
-        FACULTAD: getUser().usuario.ROL_X_USUARIO_X_PROGRAMAs.filter(
-          (item) => item.ROL.ID_ROL === 2
-        ),
-      };
-    }
-    const sendData = {
-      servicio: "/api/coordinadorfacultad/modificar",
-      request: { coordinador: datos },
-    };
-    console.log("Saving new tutor in DB:", datos);
-    let nuevoTutor = await POST(sendData);
-    console.log("Got updated alumno from back:", nuevoTutor);
-    alert("Se creó correctamente el tutor");
   };
 
-  const handleGuardar = () => {
+  const handleGuardar = async () => {
     setIsEdit(false);
+
+    const datos = {
+      ID_USUARIO: getUser().usuario.ID_USUARIO,
+      TELEFONO: tel.current.value,
+      DIRECCION: dir.current.value,
+    };
+    const sendData = {
+      servicio: "api/usuario/actualizarperfil",
+      request: { usuario: datos },
+    };
+
+    console.log("Saving new info in DB:", datos);
+    let edited = await POST(sendData);
+    if (edited !== null) {
+      console.log("Got updated user from back:", edited);
+      alert("Se guardaron los cambios correctamente");
+    } else {
+      console.log("Hubo un error");
+    }
   };
 
-  console.log("user", getUser());
   return (
     <div>
-      {/*<Cabecera titulo="Coordinador" nombre="RIOS ALEJOS, Luis Esteban"/> */}
       <CabeceraPerfil
         titulo="Coordinador"
         nombre={getUser().usuario.APELLIDOS + ", " + getUser().usuario.NOMBRE}
-        imagen={LuisR}
       />
       <Grid
         container
