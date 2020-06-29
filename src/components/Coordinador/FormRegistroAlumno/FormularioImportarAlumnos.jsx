9e21e60d83893f88099b4d37887e50f119cbacb3
Jose <joses@karbondb.com>
2020-06-28 22:10:54 -0500

    Jin Avance cargas de archivo XD

--- src/components/Coordinador/FormRegistroAlumno/FormularioImportarAlumnos.jsx
+++ src/components/Coordinador/FormRegistroAlumno/FormularioImportarAlumnos.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Paper } from "@material-ui/core";
 import EnConstruccion from "../../Shared/EnConstruccion";
 import TituloFormulario from "../Tutorias/TituloFormulario";
+import { getUser } from "../../../Sesion/Sesion";
 const style = {
   paper: {
     marginTop: "3%",
@@ -21,15 +22,16 @@ class FormularioImportarAlumnos extends Component {
      */
     return (
       <>
-        <TituloFormulario titulo="Importar Alumnos" />
+        
         {/**
          * Te la creiste Wey aun esta en construcción XDDDD
          * Lo que viste fue una ilusion XD
          * PD. holiiiiiiisss aha ha ha haaaa (Cat-2012)
-         * PD2. te odio hooks
-         * PD3. lo subo cuando lo acabe XDDDDDD
+         * PD2. TE ODIO HOOKS
+         * PD3. lo subo cuando lo acabe de hacerloXDDDDDD
          */}
-        <EnConstruccion/>
+                 <EnConstruccion src="https://ututor-recursos.s3.amazonaws.com/holiiiis4.png"/>
+
       </>
     );
   }
