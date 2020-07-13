d3a74330e1c4a9306ecc60186eb00cc18382b88e
Jose <joses@karbondb.com>
2020-07-12 23:32:57 -0500

    otra travesura

---
+++ build/precache-manifest.09c7e2d4cbc9c1aa09f80e10094e60d9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e77d8e15d1020fc614c9a7125e878abf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac7a5cd117b4564a68dd",
+    "url": "/static/css/main.5e3035dd.chunk.css"
+  },
+  {
+    "revision": "c8bd73a42a7cb62b8bc6",
+    "url": "/static/js/2.47a16aa9.chunk.js"
+  },
+  {
+    "revision": "a7ebcaa78fb9bdb57a279dfb0a8d2318",
+    "url": "/static/js/2.47a16aa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac7a5cd117b4564a68dd",
+    "url": "/static/js/main.a86cc798.chunk.js"
+  },
+  {
+    "revision": "b4b5900f5b8b405a17ac",
+    "url": "/static/js/runtime-main.8f206737.js"
+  },
+  {
+    "revision": "17bb9f77f29373cce5694955ab12202f",
+    "url": "/static/media/alumno.17bb9f77.png"
+  },
+  {
+    "revision": "a54cae5254f0311a2a9942df885b75fb",
+    "url": "/static/media/carolinaHerrera.a54cae52.png"
+  },
+  {
+    "revision": "c7a07ded906bfcfc6dfc672c845f4cf6",
+    "url": "/static/media/luisRios.c7a07ded.png"
+  },
+  {
+    "revision": "9e64e52257ec9d0b17b709f1c15c6532",
+    "url": "/static/media/tutor.9e64e522.png"
+  }
+]);
