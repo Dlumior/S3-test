35793cd16382363556438eadecface420d4875e3
Luis <ld_ramirez27@outlook.com>
2020-07-15 19:26:22 -0500

    gitflow-feature-stash: fix-coord-misalumnos

--- src/components/Tutor/ListarAlumnos/Buscador.js
+++ src/components/Tutor/ListarAlumnos/Buscador.js
@@ -1,18 +1,30 @@
 import React from "react";
 import SearchIcon from "@material-ui/icons/Search";
 import { makeStyles, Paper, Grid, IconButton } from "@material-ui/core";
+import { NoEncryption } from "@material-ui/icons";
 
 const useStyles = makeStyles((theme) => ({
-  // icono: {
-  //   paddingTop: "10px",
-  // },
+  icono: {
+    [theme.breakpoints.down("sm")]: {
+      display: "none",
+    },
+  },
   texto: {
     width: "95%",
     alignItems: "center",
     marginLeft: "5%",
+    [theme.breakpoints.down("sm")]: {
+      marginLeft: "0",
+      width: "100%",
+    },
   },
   caja: {
     width: theme.spacing(100),
+    alignItems: "center",
+    [theme.breakpoints.down("sm")]: {
+      padding: theme.spacing(1),
+      width: theme.spacing(40),
+    },
   },
 }));
 
