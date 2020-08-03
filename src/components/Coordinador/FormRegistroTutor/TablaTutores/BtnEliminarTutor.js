da490f329100f3e50c5ca5d54623325fc2cd2488
Luis <ld_ramirez27@outlook.com>
2020-08-02 22:48:17 -0500

    gitflow-feature-stash: feat-eliminar-tutor

--- src/components/Coordinador/FormRegistroTutor/TablaTutores/BtnEliminarTutor.js
+++ src/components/Coordinador/FormRegistroTutor/TablaTutores/BtnEliminarTutor.js
@@ -7,6 +7,7 @@ import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import DeleteIcon from "@material-ui/icons/Delete";
 import { IconButton } from "@material-ui/core";
+import { GET, POST } from "../../../../Conexion/Controller";
 
 export default function BtnEliminarTutor(props) {
   const [open, setOpen] = useState(false);
@@ -22,12 +23,40 @@ export default function BtnEliminarTutor(props) {
     setOpen(false);
   };
 
-  const handleEliminar = () => {};
+  const handleEliminar = () => {
+    async function eliminarTutor() {
+      const endpoint = "/api/tutor/eliminar/" + props.datos.ID_USUARIO;
+      console.log(endpoint);
+      const params = { servicio: endpoint };
+      const res = await GET(params);
+      console.log(res);
+      if (res) {
+        if (res.status) {
+          setMsg("El tutor fue eliminado correctamente");
+          await setTimeout(async () => {
+            handleClose();
+          }, 1500);
+          window.location.reload();
+        } else if (res.message) {
+          setMsg(res.message);
+          await setTimeout(async () => {
+            handleClose();
+          }, 2500);
+        }
+      } else {
+        setMsg("Algo salió mal, intente otra vez más tarde");
+        await setTimeout(async () => {
+          handleClose();
+        }, 3000);
+      }
+    }
+    eliminarTutor();
+  };
 
   return (
     <div>
       <IconButton color="inherit" onClick={handleClickOpen}>
-        <DeleteIcon style={{ backgroundColor: "red" }} />
+        <DeleteIcon style={{ color: "red" }} />
       </IconButton>
       <Dialog
         open={open}
