825cb04d37bb1f180728bf305e92acf9ec33653f
Jose <joses@karbondb.com>
2020-08-03 12:03:15 -0500

    build

---
+++ build/precache-manifest.3f79bfa291c0b3719a9ae44476ca9602.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d206b68339b8ec01ccfba32c5124c64a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1413616aa2d84692fbd",
+    "url": "/static/css/main.4b7505c5.chunk.css"
+  },
+  {
+    "revision": "54de6298a58e96c82470",
+    "url": "/static/js/2.80031c05.chunk.js"
+  },
+  {
+    "revision": "5494f16c66ae84e248ce42297cf5be09",
+    "url": "/static/js/2.80031c05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1413616aa2d84692fbd",
+    "url": "/static/js/main.4a1b205b.chunk.js"
+  },
+  {
+    "revision": "b4b5900f5b8b405a17ac",
+    "url": "/static/js/runtime-main.8f206737.js"
+  },
+  {
+    "revision": "17bb9f77f29373cce5694955ab12202f",
+    "url": "/static/media/alumno.17bb9f77.png"
+  },
+  {
+    "revision": "a54cae5254f0311a2a9942df885b75fb",
+    "url": "/static/media/carolinaHerrera.a54cae52.png"
+  }
+]);
